7bd5d5cb82bcc4d650c130bbb6a97c48979dfb14
Kresten Kjær <kresten.kjaer@LEGO.com>
2016-11-02 07:51:42 -0400

    Added suse

---
+++ lib/kitchen/driver/aws/standard_platform/suse.rb
@@ -0,0 +1,37 @@
+require "kitchen/driver/aws/standard_platform"
+
+module Kitchen
+  module Driver
+    class Aws
+      class StandardPlatform
+        class SLES < StandardPlatform
+          StandardPlatform.platforms["suse"] = self
+
+          def initialize(driver, _name, version, architecture)
+            super(driver, "suse", version, architecture)
+          end
+
+          def username
+            "root"
+          end
+
+          def image_search
+            search = {
+              "owner-id" => "013907871322",
+              "name" => "suse-sles-#{version}*"
+            }
+            search["architecture"] = architecture if architecture
+            search
+          end
+
+          def self.from_image(driver, image)
+            if image.name =~ /suse/i
+              image.name =~ /\b(\d+(\.\d+)?)/i
+              new(driver, "suse", (Regexp.last_match || [])[1], image.architecture)
+            end
+          end
+        end
+      end
+    end
+  end
+end
